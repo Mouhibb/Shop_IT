4b5c910852a069a6240b9b79248872175f569dda
unknown <trabelsi.mouhib3@gmail.com>
2021-06-15 16:14:39 +0100

    initial commit

--- frontend/src/components/layout/Footer.js
+++ frontend/src/components/layout/Footer.js
@@ -1,13 +1,64 @@
 import React, { Fragment } from 'react'
+import './Footer.css'
 
 const Footer = () => {
+    var d = new Date();
+
     return (
         <Fragment>
-            <footer className="py-1">
-                <p className="text-center mt-1">
-                    Shopping Cart - 2019-2020, All Rights Reserved
-                </p>
-            </footer>
+            <footer className="footer">
+  	 <div className="container">
+  	 	<div className="row">
+  	 		<div className="footer-col">
+  	 			<h4>company</h4>
+  	 			<ul>
+  	 				<li><a href="#">about us</a></li>
+  	 				<li><a href="#">our services</a></li>
+  	 				<li><a href="#">privacy policy</a></li>
+  	 				<li><a href="#">affiliate program</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div className="footer-col">
+  	 			<h4>get help</h4>
+  	 			<ul>
+  	 				<li><a href="#">FAQ</a></li>
+  	 				<li><a href="#">shipping</a></li>
+  	 				<li><a href="#">returns</a></li>
+  	 				<li><a href="#">order status</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div className="footer-col">
+  	 			<h4>online shop</h4>
+  	 			<ul>
+  	 				<li><a href="#">watch</a></li>
+  	 				<li><a href="#">high tech</a></li>
+  	 				<li><a href="#">shoes</a></li>
+  	 				<li><a href="#">dress</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div className="footer-col">
+  	 			<h4>follow us</h4>
+                   <ul>
+                   <li><i className="fa fa-map-marker"></i> Les berges du lac 2
+                                                Tunis, 1053
+                                            </li>
+                                            <li><i className="fa fa-phone"></i> Call Us : 27 581 140</li>
+                                            <li><a href="#"><i className="fa fa-envelope"></i>   support@shopit.com</a></li>
+  	 			</ul>
+  	 			<div className="social-links">
+  	 				<a href="#"><i className="fab fa-facebook-f"></i></a>
+  	 				<a href="#"><i className="fab fa-twitter"></i></a>
+  	 				<a href="#"><i className="fab fa-instagram"></i></a>
+  	 				<a href="#"><i className="fab fa-linkedin-in"></i></a>
+  	 			</div>
+  	 		</div>
+  	 	</div>
+  	 </div><br></br><br></br>
+       <p className="text-center ">
+                    ShopIT - © {d.getFullYear()},  All Rights Reserved
+      </p>
+  </footer>
+  
         </Fragment>
     )
 }
--- readme.md
+++ readme.md
@@ -1,12 +1,11 @@
-# ShopIT E-commerce Project
+# ShopIT تسوق Ecommerce Project
 
 > Complete E-commerce site built with Node.js, React, Redux, Express, MongoDB
 
-More about this project and course on Udemy. Course Name: [MERN Stack Full Ecommerce Site - Using React, Redux, Node.j](https://www.udemy.com/course/3678044)
 
 ### Env Variables
 
-Add your config variables values in the config.env file in backend/config folder as shown in the course.
+Add your config variables values in the config.env file in backend/config folder.
 
 ### Install Dependencies (Frontend)
 
@@ -23,7 +22,6 @@ npm i
 
 ### Seed Database
 
-Use the following commeand to put some dummy products in that database.
 Run it in the root folder.
 
 ```
